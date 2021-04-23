b059b93ff4f0068ab6d4c486a51e70f4284a6ee3
Álvaro Palma <palma.alvaro@outlook.com>
2021-04-23 18:33:40 -0300

    Susbstitui algoritmo do exercicio 5 pelo pedido no exercicio 6

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/scripts.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/scripts.js
@@ -1,13 +1,16 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let maiorValor = 5;
+let valoresImpares = 0;
 
 for (let index = 0; index < numbers.length; index += 1){
-  if (numbers[index] > maiorValor){
-      maiorValor = numbers[index];
-  } else {
-      maiorValor = maiorValor;
-  }
+  if (numbers[index] % 2 != 0){
+      valoresImpares += 1;
+  } 
 }
 
-console.log(maiorValor);
+if (valoresImpares != 0){
+    console.log(valoresImpares);
+} else {
+    console.log("nenhum valor ímpar encontrado");
+}
+
