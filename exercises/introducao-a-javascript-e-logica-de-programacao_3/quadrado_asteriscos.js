9e129d2bf189d8b1bd362c6a9c4d75b6df5f793e
Álvaro Palma <palma.alvaro@outlook.com>
2021-04-26 17:57:13 -0300

    Implementa o algoritmo do quadrado de asteriscos

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/quadrado_asteriscos.js
@@ -0,0 +1,12 @@
+let n = 8;
+
+let quadrado = " ";
+
+for (index = 1; index <= n; index += 1) {
+    quadrado = quadrado + ('*');
+}
+
+for (index = 1; index <= n; index += 1) {
+    console.log(quadrado);
+}
+
