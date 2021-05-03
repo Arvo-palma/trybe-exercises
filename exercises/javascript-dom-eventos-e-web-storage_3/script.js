58c99b0e885e10a83726b2935c7ae44f6e7596ca
Álvaro Palma <palma.alvaro@outlook.com>
2021-05-03 19:08:52 -0300

    Adiciona as modificaçoes do exercicio 1 ao codigo

--- exercises/javascript-dom-eventos-e-web-storage_3/script.js
+++ exercises/javascript-dom-eventos-e-web-storage_3/script.js
@@ -12,5 +12,33 @@ function createDaysOfTheWeek() {
   };
   
   createDaysOfTheWeek();
-  
-  // Escreva seu código abaixo.
+
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const daysList = document.querySelector('#days');
+
+  function createDays() {
+    for (let index = 0; index < dezDaysList.length; index += 1) {
+        const dezDays = dezDaysList[index];
+        const daysItem = document.createElement('li');
+        daysItem.className = 'day';
+        daysItem.innerHTML = dezDays;
+
+        daysList.appendChild(daysItem);
+    };
+};
+
+createDays();
+
+for (let index = 0; index < dezDaysList.length; index += 1) {
+    if (dezDaysList[index] === 24 | dezDaysList[index] === 31) {
+        document.querySelectorAll('.day')[index].classList.add('day', 'holiday');
+    }
+}
+
+for (let index = 0; index < dezDaysList.length; index += 1) {
+    if (dezDaysList[index] === 4 | dezDaysList[index] === 11 | dezDaysList[index] === 18) {
+        document.querySelectorAll('.day')[index].classList.add('day', 'friday');
+    }
+}
+
+document.querySelectorAll('.day')[dezDaysList.indexOf(25)].classList.add('day', 'holiday', 'friday');
