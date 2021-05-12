d42fa2338657f8df57fe7340bd77cf5d084e2aaa
Álvaro Palma <palma.alvaro@outlook.com>
2021-05-12 13:50:53 -0300

    Adiciona alteraçoes dos itens 1 e 2 do exercicio do dia

---
+++ exercises/html-e-css-forms-flexbox-e-responsivo_1/script.js
@@ -0,0 +1,10 @@
+function addState() {
+  const brazilStates = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
+  for(let index = 0; index < brazilStates.length; index += 1) {
+    let option = document.createElement('option');
+    option.innerText = brazilStates[index];
+    document.querySelector('#state').appendChild(option);
+  }
+}
+
+addState();
