6b8eb3b973197cb5c02fc217e5b2770746b5a183
Álvaro Palma <palma.alvaro@outlook.com>
2021-05-20 19:56:55 -0300

    Adiciona resoluçao at'eo item 6 da parte 2

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_2/parte2.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_2/parte2.js
@@ -5,15 +5,51 @@ const lesson1 = {
     turno: 'manhã',
   };
   
-  const lesson2 = {
+const lesson2 = {
     materia: 'História',
     numeroEstudantes: 20,
     professor: 'Carlos',
   };
-  
-  const lesson3 = {
+
+const lesson3 = {
     materia: 'Matemática',
     numeroEstudantes: 10,
     professor: 'Maria Clara',
     turno: 'noite',
   };
+
+const addTurno = (lesson, key, valor) => {
+    lesson[key] = valor;
+  }
+
+addTurno(lesson2, 'turno', 'noite');
+
+const keyList = (object) => {
+    console.log(Object.keys(object));
+  }
+
+const objectLength = (object) => {
+    console.log(Object.keys(object).length);
+  }
+
+const objectValues = (object) => {
+    console.log(Object.values(object));
+  }
+
+const allLessons = {};
+
+allLessons['lesson1'] = lesson1;
+allLessons['lesson2'] = lesson2;
+allLessons['lesson3'] = lesson3;
+
+const totalEstudantes = (allLessons) => {
+  const totalEstudantes = allLessons.lesson1.numeroEstudantes + allLessons.lesson2.numeroEstudantes + allLessons.lesson3.numeroEstudantes;
+  return totalEstudantes;
+}
+
+const getValueByNumber = (object, position) => {
+  const value = object[position];
+  return value;
+}
+
+console.log(lesson1[0]);
