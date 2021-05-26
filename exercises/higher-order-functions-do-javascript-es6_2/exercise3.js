2b568877da6d0714cc0168c91e6e5d20e3af8374
Álvaro Palma <palma.alvaro@outlook.com>
2021-05-26 18:29:25 -0300

    Implementa a funcao do exercicio 3

--- exercises/higher-order-functions-do-javascript-es6_2/exercise3.js
+++ exercises/higher-order-functions-do-javascript-es6_2/exercise3.js
@@ -63,4 +63,20 @@ const books = [
   },
 ];
 
-// Adicione o código do exercício aqui:
+const expectedResult = {
+  author: {
+    birthYear: 1948,
+    name: 'George R. R. Martin',
+  },
+  genre: 'Fantasia',
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  releaseYear: 1991,
+};
+
+function getNamedBook() {
+  const namedBook = books.find((book) => book.name.length === 26);
+  return namedBook;
+}
+
+assert.deepStrictEqual(getNamedBook(), expectedResult);
